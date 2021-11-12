b7cb922f63b7aaf9189128b26c0e2e490d0ba697
Pete Higson <peter.higson@gmail.com>
2021-11-12 09:51:36 +0000

    Add files via upload

---
+++ 5.installApps_Gaming.cmd
@@ -0,0 +1,22 @@
+REM HW MONITOR/BENCHMARK
+choco install -y -r gpu-z
+choco install -y -r cpu-z
+choco install -y -r pci-z
+choco install -y -r hwmonitor
+choco install -y -r msiafterburner
+choco install -y -r crystaldiskmark
+choco install -y -r aida64-extreme
+
+REM HW Drivers
+choco install -y -r geforce-experience
+choco install -y -r intel-dsa
+choco install -y -r ds4windows
+
+REM LAUNCHERS
+choco install -y -r gamesavemanager
+choco install -y -r steam
+choco install -y -r epicgameslauncher
+choco install -y -r goggalaxy
+choco install -y -r ubisoft-connect
+choco install -y -r bethesdanet
+
