d3efced7f64af4d92794da65127dec65e6655ebe
Pete Higson <peter.higson@gmail.com>
2021-11-12 09:35:27 +0000

    Update 2.installApps_Baseline.cmd
    Cleaned up uneccesary apps

--- 2.installApps_Baseline.cmd
+++ 2.installApps_Baseline.cmd
@@ -1,3 +1,5 @@
+REM Purpose of this script is to install a set of apps that all builds would find useful - standard utilities and QoL apps
+
 REM - @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 choco feature enable -n allowGlobalConfirmation
 
@@ -8,13 +10,12 @@ REM Install Web Browsers
 choco install -y -r firefox googlechrome
 
 REM Install Utilities
-choco install -y -r 7zip unetbootin putty notepadplusplus greenshot paint.net tor-browser win32diskimager autoruns filezilla
+choco install -y -r 7zip unetbootin putty notepadplusplus greenshot paint.net win32diskimager autoruns filezilla
 
 REM - Additional fonts for monotype
 choco install -y -r hackfont droidsansmono inconsolata dejavufonts
 
-REM Auto Dark Mode App
-choco install -y -r auto-dark-mode
+
 
 
  
