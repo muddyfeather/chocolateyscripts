aeb063ee4081b52e6e8d3fa5779dd98e80111672
Pete Higson <peter.higson@gmail.com>
2021-11-12 09:51:19 +0000

    Update 4.installApps_Personal.cmd

--- 4.installApps_Personal.cmd
+++ 4.installApps_Personal.cmd
@@ -1,9 +1,46 @@
-choco install -y -r  audacity handbrake attributechanger mediainfo openvpn rufus
-choco install -y -r  vlc gimp imgburn filebot yumi skype telegram calibre deluge picard
-choco install -y -r  youtube-dl ffmpeg win-youtube-dl opencodecs
-choco install -y -r  heidisql sdformatter smartty
+REM Install media tools
+choco install -y -r  audacity handbrake attributechanger mediainfo
+choco install -y -r  vlc gimp filebot calibre deluge picard
+choco install -y -r  youtube-dl win-youtube-dl youtube-dl-gui
+
+REM Install codes
+choco install -y -r ffdshow ffmpeg opencodecs vp8-vfw
 
 REM Install Android Debug Bridge
 choco install -y -r adb
 
-choco install -y -r cdex
+REM Install ChocolateyGUI
+choco install -y -r chocolateygui
+
+REM Install Bitwarden Password Manager
+choco install -y -r bitwarden
+choco install -y -r bitwarden-cli
+
+choco install -y -r bulkrenameutility
+
+REM Hardware Support
+choco install -y -r logitech-options
+choco install -y -r unifying
+choco install -y -r icue
+
+REM Install Ventoy USB Bootable Creator & other USB tools
+choco install -y -r ventoy
+
+REM Install web copier tools
+choco install -y -r cyotek-webcopy
+
+REM Install WhatsApp Desktop
+choco install -y -r whatsapp
+
+REM Install picotorrent client (For ad-hoc downloads)
+choco install -y -r picotorrent
+
+REM Install Download Manager
+choco install -y -r jdownloader
+
+REM install Google Drive
+choco install -y -r google-drive-file-stream
+
+REM Install partition management tools
+choco install -y -r diskgenius
+choco install -y -r partitionwizard
