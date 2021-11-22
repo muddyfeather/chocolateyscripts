d58fd3c6f3f2ac4173b2c7c48808d8df762ca813
Pete Higson <7252198+peterhigsonuk@users.noreply.github.com>
2021-11-22 20:37:01 +0000

    Update 3.installApps_Work.cmd
    added jabra direct for headsets (work) and ad-tidy free edition

--- 3.installApps_Work.cmd
+++ 3.installApps_Work.cmd
@@ -4,12 +4,12 @@ REM Install GNU and Support utilities
 choco install -y -r nmap nano whoiscl winscp winmerge rvtools
 
 REM Install Work applications and utilities
-choco install -y -r SwissFileKnife markdownpad2
-choco install -y -r dupeguru cyberduck adexplorer
+choco install -y -r SwissFileKnife markdownpad2 dupeguru cyberduck
 choco install -y -r sql-server-management-studio 
 choco install -y -r azure-data-studio
 choco install -y -r syncback veracrypt wireshark windirstat
-choco install -y -r ad-photo-edit-free pwgen
+choco install -y -r ad-photo-edit-free
+choco install -y -r ad-tidy-free
 
 REM Install Remote Sysadmin Tools
 choco install -y -r rsat
@@ -28,6 +28,9 @@ choco install -y -r microsoft-teams
  
 REM Install a specific version of RoyalTS v2 
 choco install -y -r royalts -version 2.2.7.60519
+
+REM Install Jabra Direct for Headsets
+choco install -y -r jabra-direct
  
 
 
